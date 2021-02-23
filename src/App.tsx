6a7b7f4c31ec03b83e041bd304bc2ad0bd5c9d4d
iamtheluiz <iamtheluiz.dev@gmail.com>
2021-02-22 23:17:31 -0300

    feat: add main screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import './styles/global.css';
+
 function App() {
   return (
-    <h1>Horse Race!</h1>
+    <main>
+      <section id="menu">
+        <h1>Horse Race!</h1>
+        <span className="subtitle">Are you ready? Make your bets!</span>
+        <button className="button outline" id="start-game">Start!</button>
+      </section>
+    </main>
   );
 }
 
