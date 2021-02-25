e6bf7fd8b7e70a496d5d7c62a6fbc71f07fe334a
iamtheluiz <iamtheluiz.dev@gmail.com>
2021-02-24 21:42:54 -0300

    feat: add money balance structure

--- src/pages/Bet/index.tsx
+++ src/pages/Bet/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useHorse } from "../../contexts/horse";
+import { useHistory } from "react-router-dom";
 
 import Button from "../../components/Button";
 import HorseItem from "../../components/HorseItem";
@@ -8,22 +9,30 @@ import "./styles.css";
 
 const Bet: React.FC = () => {
   const { horses } = useHorse();
+  const history = useHistory();
 
   return (
     <section id="bet">
-      <h1>Make Your Bet!</h1>
+      <header>
+        <Button style={{ backgroundColor: "#cc4242" }} onClick={() => history.push("/")}>Menu</Button>
+        <h1>Make Your Bet!</h1>
+        <div className="money">
+          <div className="icon">$</div>
+          <span>3.00</span>
+        </div>
+      </header>
       <div className="content">
         <div className="horse-list">
           {horses.map(horse => (
             <HorseItem key={horse.name} horse={horse} />
           ))}
         </div>
-        <footer>
-          <span>R$</span>
-          <input className="input-text" type="text" />
-          <Button style={{ backgroundColor: "#93cc42" }}>Bet!</Button>
-        </footer>
       </div>
+      <footer>
+        <span>$</span>
+        <input className="input-text" type="number" />
+        <Button style={{ backgroundColor: "#93cc42" }}>Bet!</Button>
+      </footer>
     </section>
   );
 };
